e5a79dea3702256ace57b07cd976dec5111523e4
Patrick GIRY <patrick.giry@vidal.fr>
2017-03-13 13:00:20 +0100

    Fix suppress CR

--- tocrc.sh
+++ tocrc.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
+oldIfs=$FS
+IFS=$'\n'
 input=$(cat)
 className=`grep "class" <<< "$input" | awk '{printf "%s", $3}'`
-fields=`grep "private" <<< "$input" | grep -v "(" | sed 's/;//g' | sed "s/final//g" | awk '{printf "%s: %s\n", $3, $2}'`
+fields=`grep "private" <<< "$input" | grep -v "(" | sed 's/;//g' | sed "s/\r//g" | sed "s/final//g" | awk '{printf "%s: %s\n", $3, $2}'`
 
 echo "<table>"
 echo "    <tr>"
 echo "        <th colspan=\"2\">$className</th>"
 echo "    </tr>"
 echo "    <tr><td>"
-oldIfs=$FS
-IFS=$'\n'
 for field in $fields
 do echo "        $field <br/>"
 done
-IFS=$oldIfs
 echo "    </td><td>"
 echo "    </td></tr>"
 echo "</table>"
 echo
+IFS=$oldIfs
 
