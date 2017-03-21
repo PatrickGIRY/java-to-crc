e4b2efd4500d7671cbbf9c71251094dca4271657
Patrick GIRY <patrick.giry@vidal.fr>
2017-03-21 18:12:34 +0100

    add type link

--- tocrc.sh
+++ tocrc.sh
@@ -4,11 +4,11 @@ oldIfs=$FS
 IFS=$'\n'
 input=$(cat)
 className=`grep "class" <<< "$input" | awk '{printf "%s", $3}'`
-fields=`grep "private" <<< "$input" | grep -v "(" | sed 's/;//g' | sed "s/\r//g" | sed "s/final//g" | awk '{printf "%s: %s\n", $3, $2}'`
+fields=`grep "private" <<< "$input" | grep -v "{" | sed 's/;//g' | sed "s/\r//g" | sed "s/final//g" | awk '{printf "%s: <a href=\"#%s\">%s</a>\n", $3, $2, $2}'`
 
 echo "<table>"
 echo "    <tr>"
-echo "        <th colspan=\"2\">$className</th>"
+echo "        <th colspan=\"2\"><a name=\"$className\"></a>$className</th>"
 echo "    </tr>"
 echo "    <tr><td>"
 for field in $fields
